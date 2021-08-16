ff5e5a744a88224df7593d619636fc95690355f4
ParkerThomasScott <scottpt@uci.edu>
2021-08-16 02:51:56 -0700

    initial port

---
+++ src/main/java/Main.java
@@ -0,0 +1,62 @@
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+import uk.ac.starlink.table.StarTable;
+import uk.ac.starlink.table.StarTableOutput;
+import uk.ac.starlink.task.Task;
+import uk.ac.starlink.task.TaskException;
+import uk.ac.starlink.ttools.task.MapEnvironment;
+import uk.ac.starlink.ttools.task.TablePipe;
+import uk.ac.starlink.ttools.task.CdsUploadSkyMatch;
+
+public class Main {
+
+    public static void main(String[] args) throws TaskException, IOException{
+        // Get stilts command
+        if( args.length < 1) {
+            System.err.println("\n Error. No args: " + Main.class.getName());
+            System.exit(1);
+        }
+        /*
+        cdstable=I/350/gaiaedr3
+        find=all
+        ifmt=csv
+        in=Keck_2021-08-09_UT-CandidatesForObservation-HighPriorityCandidates.csv
+        ra=RA
+        dec=Dec
+        radius=4
+        out=TargetsWithGaiaEDR3counterparts.csv*/
+
+        System.out.println(Arrays.toString(args));
+
+        // Set up and populate execution environ w/ param values
+        MapEnvironment env = new MapEnvironment();
+
+        for(String arg : args) {
+            String[] argSplit = arg.split("=", 0);
+            System.out.println(Arrays.toString(argSplit));
+            switch (argSplit[0]) {
+                case "cdstable" -> env.setValue("cdstable", argSplit[1]);
+                case "find" -> env.setValue("find", argSplit[1]);
+                case "ifmt" -> env.setValue("ifmt", argSplit[1]);
+                case "ra" -> env.setValue("ra", argSplit[1]);
+                case "dec" -> env.setValue("dec", argSplit[1]);
+                case "radius" -> env.setValue("radius", argSplit[1]);
+                case "in" -> env.setValue("in", argSplit[1]);
+                case "out" -> env.setValue("out", argSplit[1]);
+            }
+        }
+
+        // Create and execute task
+        Task cdsskyTask = new CdsUploadSkyMatch();
+        cdsskyTask.createExecutable(env).execute();
+
+        StarTable outTable = env.getOutputTable("omode");
+
+        // TODO: Not sure if csv is valid format...
+        new StarTableOutput().writeStarTable(outTable, "starOutput","ascii");
+
+    }
+}
